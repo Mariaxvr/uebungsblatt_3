902b63f2de69fbf79d53f742fdccbf43460bdb30
Mariaxvr <mariavkrohnq@gmail.com>
2025-11-11 11:30:40 +0100

    Hausaufgabe 3

--- src/h3/H3_Main.java
+++ src/h3/H3_Main.java
@@ -3,23 +3,23 @@ package h3;
 public class H3_Main {
     public static void main(String[] args) {
         int max = 10; // max plätze
-        int fix = 5; // platz erhalten
+        int fix = 10; // platz erhalten
         int wartend = 5; // leute auf warteliste
-        boolean istVoll = false;
+        boolean istVoll = true;
 
-        if (max-fix > 0 && wartend > 0) {
+        if (max-fix > 0 && wartend >= 0) {
 
             if (wartend <= max-fix) {
                 fix = fix + wartend;
                 wartend = 0;
             }
             else if ( wartend > max-fix) {
-            wartend = wartend - (max-fix);
-            fix = max;
+                wartend = wartend - (max - fix);
+                fix = max;
             }
             if (fix==max)
                 istVoll = true;
-            else
+            else if (fix!=max)
                 istVoll = false;
 
 
